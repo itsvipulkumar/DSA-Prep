8e1fcbbd2e8ef11f43451b721504b7086e4f7892
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-02-14 00:17:42 +0530

    Update RotateArray.cpp

--- LeetCode/RotateArray.cpp
+++ LeetCode/RotateArray.cpp
@@ -1,16 +1,16 @@
 // C++ program to demonstrate
 // Implementation of erase() function
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    vector<int> nums{1,2,3,4,5,6,7};
+    vector<int> nums{-1,-100,3,99};
     vector<int>::iterator it1, it2;
-    int n = nums.size();
+    // int n = nums.size();
     vector<int> temp;
-    int k = 3;
+    int k = 2;
     // k = k + 1;
     // for (int i = k+1; i < n; i++)
     //     temp.push_back(nums[i]);
@@ -24,26 +24,39 @@ int main()
     // {
     //     nums.push_back(temp[i]);
     // }
-    int p = k;
-    while (p != 0)
-    {
-        temp.push_back(nums[n - p]);
-        p--;
-        nums.pop_back();
-    }
-    for (int i = 0; i < nums.size(); i++)
-    {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-    for (int i = temp.size()-1; i>=0; i--)
-    {
-        // nums.push_back(temp[i]);
-        nums.insert(nums.begin(),temp[i]);
-    }
-    for (int i = 0; i < nums.size(); i++)
-    {
-        cout << nums[i] << " ";
-    }
+    // int p = k;
+    // while (p != 0)
+    // {
+    //     temp.push_back(nums[n - p]);
+    //     p--;
+    //     nums.pop_back();
+    // }
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     cout << nums[i] << " ";
+    // }
+    // cout << endl;
+    // for (int i = temp.size()-1; i>=0; i--)
+    // {
+    //     // nums.push_back(temp[i]);
+    //     nums.insert(nums.begin(),temp[i]);
+    // }
+    // for (int i = 0; i < nums.size(); i++)
+    // {
+    //     cout << nums[i] << " ";
+    // }
+
+
+     int n = nums.size();
+     k=k%n;
+ reverse(nums.begin(),nums.begin()+n-k);
+ reverse(nums.begin()+n-k,nums.end());
+reverse(nums.begin(),nums.end());
+for(int i=0;i<n;i++)
+{
+    cout<<nums[i]<<" ";
+}
+
+
     return 0;
 }
