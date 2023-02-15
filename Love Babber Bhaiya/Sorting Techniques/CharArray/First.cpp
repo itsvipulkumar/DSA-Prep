58b0471fe779df496b39e9fd28b54749a0972c4d
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-02-15 22:58:02 +0530

    Create First.cpp

---
+++ Love Babber Bhaiya/Sorting Techniques/CharArray/First.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // creation the char array 
+
+    char arr[10]="Vipul";
+
+    // cin>>arr; 
+    cout<<arr;
+    cout<<endl<<"Length of char arr : "<<strlen(arr);
+    return 0;
+}
